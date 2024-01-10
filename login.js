3ba167c4ae83bdcfbdb3264d57328c2e062bacf4
HaxhiZogaj <150356532+HaxhiZogaj@users.noreply.github.com>
2024-01-10 18:26:46 -0500

    krijimi prej fillimit te js pershkak te disa ndryshimeve ne html-form
    This reverts commit d00adf5713a42e9f42abeca7ebc3006bff7fca2d.

--- login.js
+++ login.js
@@ -1,17 +1,15 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    let btnLogin = document.getElementById("login");
-    let btnSignUp = document.getElementById("signup");
+let btnLogin = document.getElementById("login");
+let btnSignUp = document.getElementById("signup");
 
-    let signIn = document.querySelector(".signin");
-    let signUp = document.querySelector(".signup");
+let signIn = document.querySelector(".signin");
+let signUp = document.querySelector(".signup");
 
-    btnLogin.onclick = function(){
-        signIn.classList.add("active");
-        signUp.classList.add("inActive");
-    }
+btnLogin.onclick = function(){
+    signIn.classList.add("active");
+    signUp.classList.add("inActive");
+}
 
-    btnSignUp.onclick = function(){
-        signIn.classList.remove("active");
-        signUp.classList.remove("inActive");
-    }
-});
+btnSignUp.onclick = function(){
+   signIn.classList.remove("active");
+   signUp.classList.remove("inActive");
+};
